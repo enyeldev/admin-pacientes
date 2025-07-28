211aac226f319aa95b5a476bbd5c613834dde662
Enyel Medrano <medranogarciaenyel@gmail.com>
2025-07-28 01:24:19 -0400

    add: signup

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -2,7 +2,7 @@ type ErrorProps = {
   children: React.ReactNode;
 };
 
-export const Error = ({ children }: ErrorProps) => {
+export const ErrorComponent = ({ children }: ErrorProps) => {
   return (
     <p className="text-center my-2 bg-red-600 text-white font-bold text-sm uppercase p-2 rounded-sm">
       {children}
