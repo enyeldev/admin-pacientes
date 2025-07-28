e9bde03dcb824fdf8516c53f0689880a172469ce
Enyel Medrano <medranogarciaenyel@gmail.com>
2025-07-28 03:03:14 -0400

    add: patient list edit and delete

--- src/models/dashboard/layouts/DashboardLayout.tsx
+++ src/models/dashboard/layouts/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 export const DashboardLayout = () => {
   return (
@@ -6,6 +7,7 @@ export const DashboardLayout = () => {
       <div className="">
         <Outlet />
       </div>
+      <ToastContainer />
     </>
   );
 };
